eea6b4ddc0e5531c64da7750d96ee2e00fb0cba8
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2024-04-08 17:00:36 -0300

    add comments

--- utils/mint.ts
+++ utils/mint.ts
@@ -1,17 +1,21 @@
 import { ethers } from 'ethers';
 import contractAbi from "./contract.json";
 
+// Environment variables for network configuration and contract interaction
 const rpcUrl = process.env.NEXT_PUBLIC_KAKAROT_SEPOLIA_URL;
-
 const contractAddress = process.env.NEXT_PUBLIC_KAKAROT_SEPOLIA_CONTRACT_ADDRESS as `0x`;
 
+// Initialize provider and contract using ethers.js
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 const contract = new ethers.Contract(contractAddress, contractAbi.abi, provider);
 export async function mintNft(toAddress: string, privateKey: any) {
   try {
+
+    // Creating a wallet with the private key and provider to sign transactions
     const wallet = new ethers.Wallet(privateKey, provider);
     const contractWithSigner = contract.connect(wallet) as any;
 
+    // Executing the safeMint function of the contract to mint NFTs
     const transaction = await contractWithSigner.mintNFT(toAddress);
     console.log("transaction", transaction);
 
@@ -24,9 +28,10 @@ export async function mintNft(toAddress: string, privateKey: any) {
 
 export async function balanceOf(address: string) {
     try {
+      // Querying the balance and the contract owner for logging purposes
       const balance = await contract.balanceOf(address);
-      const owner = await contract.owner();
       return balance.toString();
+      
     } catch (error) {
       console.log(error);
       return error;
